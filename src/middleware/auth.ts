0179c2a42db5df9170f46613b0224b1a0ed87c79
Arsgn <blackbarber1323@gmail.com>
2026-04-16 14:52:43 +0600

    admin

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 
-export const checkAdmin = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const checkAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (process.env.NODE_ENV !== "production") {
+    return next();
+  }
+
   const token = req.headers.authorization;
 
-  if (token !== "Bearer admin123") {
+  const ADMIN_TOKEN = process.env.ADMIN_TOKEN || "admin123";
+
+  if (token !== `Bearer ${ADMIN_TOKEN}`) {
     return res.status(401).json({ error: "Нет доступа" });
   }
 
